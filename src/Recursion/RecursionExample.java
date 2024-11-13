9754115878bacc0e5586b716000363f0a4046875
macman1717 <cgriffin10@ggc.edu>
2024-11-13 08:43:38 -0500

    added Nov 11 classwork

--- src/Recursion/RecursionExample.java
+++ src/Recursion/RecursionExample.java
@@ -5,12 +5,32 @@ public class RecursionExample {
         int number = 5;
         System.out.println("From recursive method: " + getFactorial(number));
         System.out.println("From hard coded: " + 5 * 4 * 3 * 2 * 1);
+
+        System.out.println(getFib(1));
+        System.out.println(getFib(4));
+        System.out.println(getFib(10));
     }
 
     public static int getFactorial(int x){
         if(x == 1){
             return 1;
         }
-        return x * getFactorial(x);
+        return x * getFactorial(x - 1);
+    }
+
+    /*
+    getFib(1) = 0;
+    getFib(4) = 2;
+    getFib(10) = 34;
+     */
+    public static int getFib(int x){
+        if(x == 1){
+            return 0;
+        }
+        else if(x == 2){
+            return 1;
+        }
+        return getFib(x-1)+getFib(x-2);
+
     }
 }
